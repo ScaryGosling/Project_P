37370a630b38d6671b4a36df78aa4365344fd15e
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-10-04 13:57:46 +0200

    rotation

--- PROJ_P/Assets/Scripts/Movement/PlayerMovement.cs
+++ PROJ_P/Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,21 +2,26 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    RaycastHit raycastHit;
+    Ray ray;
+    Vector3 newPos;
+    Quaternion rotation;
+    [SerializeField] private float rotationSpeed = 8;
 
     public void Update()
     {
         SetRotation();
     }
-
     public void SetRotation() {
 
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if(Physics.Raycast(ray, out hit))
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out raycastHit))
         {
-            Debug.Log(hit.point);
-            transform.eulerAngles = transform.eulerAngles
+            newPos = raycastHit.point - transform.position;
+            newPos.y = 0;
+            rotation = Quaternion.LookRotation(newPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
         }
 
 
