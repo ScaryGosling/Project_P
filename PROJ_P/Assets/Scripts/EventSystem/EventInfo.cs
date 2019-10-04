75ddb237bdd09f1f8e49e160af3f762171e59136
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-10-04 11:31:27 +0200

    eventsystem

---
+++ PROJ_P/Assets/Scripts/EventSystem/EventInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EventInfo
+{
+    public string eventDescription;
+}
+
+public class DebugEvent : EventInfo
+{
+    public int VerbosityLvl;
+}
+
+
+#region ListenerDraft
+
+//public class WinListener : MonoBehaviour
+//{
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        EventSystem.Current.RegisterListener<WinningEvent>(Winning);
+//    }
+
+//    // Update is called once per frame
+//    void Winning(WinningEvent winEvent)
+//    {
+//        Debug.Log("you won! uwu");
+//    }
+//}
+#endregion
