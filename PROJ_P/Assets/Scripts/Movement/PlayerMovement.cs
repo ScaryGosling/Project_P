c93e505c5b2681ce795fd889cb0da61fe2a276c5
alexancast <37774672+alexancast@users.noreply.github.com>
2019-10-04 13:39:28 +0200

    Basic scene and rotation

---
+++ PROJ_P/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public void Update()
+    {
+        SetRotation();
+    }
+
+    public void SetRotation() {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.point);
+            transform.eulerAngles = transform.eulerAngles
+        }
+
+
+    }
+
+
+
+}
