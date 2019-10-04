e41a8f1b4bd255233bb6d58fe47daf4d1507212a
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-10-04 14:19:51 +0200

    camera follows

---
+++ PROJ_P/Assets/Scripts/GeneralScripts/CameraBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        offset.x = transform.position.x - player.transform.position.x;
+        offset.y = transform.position.y - player.transform.position.y;
+        offset.z = transform.position.z - player.transform.position.z;
+    }
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref velocity, 0.3f);
+    }
+}
