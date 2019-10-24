c035a0de371d0a4c0fff597f3c9a273d3ce6a5e7
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2019-10-24 11:07:18 +0200

    JustPushing

---
+++ PROJ_P/Assets/Scripts/AI/Opponents/RusherChase1.cs
@@ -0,0 +1,35 @@
+﻿//Main Author: Emil Dahl
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/RusherChase")]
+public class RusherChase : ChaseBase
+{
+    public override void EnterState()
+    {
+        base.EnterState();
+    }
+
+    public override void ToDo()
+    {
+        base.ToDo();
+        if (owner.player != null && owner.agent.enabled)
+        {
+            Chase();
+            CheckForDamage();
+        }
+    }
+}
+
+#region ChaseLegacy
+// lightAngle = lightField.spotAngle;
+//ChaseEvent chaseEvent = new ChaseEvent();
+//chaseEvent.gameObject = owner.gameObject;
+//chaseEvent.eventDescription = "Chasing Enemy";
+//chaseEvent.audioSpeaker = audioSpeaker;
+
+//EventSystem.Current.FireEvent(chaseEvent);
+#endregion
