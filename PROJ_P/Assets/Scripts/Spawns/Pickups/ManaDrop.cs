d567ea1b2f4a2a2db7900073e6f322f31675cd37
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2019-10-30 16:48:32 +0100

    Potion based on class

--- PROJ_P/Assets/Scripts/Spawns/Pickups/ManaDrop.cs
+++ PROJ_P/Assets/Scripts/Spawns/Pickups/ManaDrop.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class ManaDrop : Pickup
 {
-    
+    [SerializeField] private Material[] materials;
+    protected override void Start()
+    {
+        base.Start();
+        SetMaterials();
+    }
     protected override void DoSomething()
     {
         player.Resource.IncreaseResource(fillAmount);
         GameObject.Destroy(gameObject);
+    }
+
+    private void SetMaterials()
+    {
+        if (player.GetComponent<Player>().playerClass == PlayerClass.WIZARD)
+            gameObject.GetComponent<MeshRenderer>().material = materials[0];
+        else
+            gameObject.GetComponent<MeshRenderer>().material = materials[1];
     }
 }
