9385e53d951c2995ef5ac4c7d4d511ec77d2f354
Alexander Castman <alca3002@win.dsv.su.se>
2019-11-06 16:44:11 +0100

    Warrior shield

---
+++ PROJ_P/Assets/Scripts/Attacks/Warrior/LifeLeech.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeLeech : PlayerAttack
+{
+    private GameObject addedParticle;
+
+    public override void Execute()
+    {
+        base.Execute();
+    }
+
+    public override void RunAttack()
+    {
+        base.RunAttack();
+
+        
+        
+    }
+
+
+}
---
+++ PROJ_P/Assets/Scripts/Attacks/Warrior/Shield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Attacks/Warrior/Shield")]
+public class Shield : PlayerAttack
+{
+    [SerializeField] private GameObject shieldPrefab;
+    [SerializeField] private float duration;
+    private GameObject shield;
+
+    public override void RunAttack()
+    {
+        base.RunAttack();
+        shield = Instantiate(shieldPrefab, Player.instance.transform);
+        shield.transform.position += new Vector3(0,0.5f,0);
+        shield.AddComponent<Timer>().RunCountDown(duration, RemoveShield, Timer.TimerType.DELAY);
+    }
+
+
+    public void RemoveShield()
+    {
+    }
+
+}
