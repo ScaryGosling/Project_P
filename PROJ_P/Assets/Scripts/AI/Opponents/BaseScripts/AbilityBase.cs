b2105e220dc3e49113357e86483d85deb2730eb1
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2019-11-18 15:10:18 +0100

    Start of jump

---
+++ PROJ_P/Assets/Scripts/AI/Opponents/BaseScripts/AbilityBase.cs
@@ -0,0 +1,45 @@
+﻿//Main Author: Emil Dahl
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+/// <summary>
+/// States that inherit from this class share some sort behavior in the form of a special ability.
+/// </summary>
+[RequireComponent(typeof(CapsuleCollider))]
+public class AbilityBase: AliveBase
+{
+    public override void EnterState()
+    {
+        base.EnterState();
+    }
+
+    public override void ToDo()
+    {
+        base.ToDo();
+        ExecuteAbility();
+    }
+    
+    protected override void CheckForDamage() { }
+
+    /// <summary>
+    /// Decides which type of ability should be used in given situation.
+    /// </summary>
+    protected virtual void ExecuteAbility() { }
+
+    }
+#region EnemyBaseLegacy
+// lightTreshold = owner.LightThreshold;
+//     spreadAngle = Quaternion.AngleAxis(lightField.spotAngle, owner.agent.velocity);
+//// protected float lightAngle;
+// //private Quaternion spreadAngle;
+//protected float DotMethod()
+//{
+//    heading = (owner.player.transform.position - owner.transform.position).normalized;
+//    dotProduct = Vector3.Dot(owner.agent.velocity.normalized, heading);
+//    return dotProduct;
+//}
+#endregion
---
+++ PROJ_P/Assets/Scripts/AI/Opponents/ChaseScripts/JumpImpact.cs
@@ -0,0 +1,99 @@
+﻿//Main Author: Emil Dahl
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(menuName = "Hostile/Boomer/BoomerChase")]
+public class JumpImpact : AbilityBase
+{
+    private GameObject jumpTimer, graphicalPrefab;
+    private float jumpWindupTime = 3f;
+    private float jumpSpeed = 5f, jumpHeight = 4f;
+    private Vector3 playerPositionalDelay;
+    private Vector3 direction;
+    private Vector3 movement;
+
+    public override void EnterState()
+    {
+        base.EnterState();
+        owner.agent.ResetPath();
+        //Try changing to stopped
+        owner.agent.enabled = false;
+
+        Instantiate(graphicalPrefab, playerPositionalDelay, Quaternion.identity);
+        //Don't forget to check so target is player 
+        playerPositionalDelay = owner.target.transform.position;
+        //Likely to land too far above the ground // This is wrong, says unit should jump from above where it is currently standing
+        direction = ((new Vector3(owner.transform.position.x, jumpHeight, owner.transform.position.z)) - owner.target.transform.position);
+    }
+
+    public override void ToDo()
+    {
+        base.ToDo();
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+        owner.ChangeState<BoomerDeath>();
+    }
+
+    public override void TakeDamage(float damage, float magnitude)
+    {
+        base.TakeDamage(damage, magnitude);
+
+        float oldHealth = owner.Health;
+        owner.Health -= damage;
+        owner.ui.ChangeHealth(owner.InitialHealth, owner.Health);
+    }
+
+    protected override void ExecuteAbility()
+    {
+        base.ExecuteAbility();
+        jumpTimer = Instantiate(new GameObject("JumpTimer"));
+        jumpTimer.AddComponent<Timer>().RunCountDown(jumpWindupTime, Jump, Timer.TimerType.DELAY);
+    }
+
+    private void Jump()
+    {
+        movement = direction * jumpSpeed;
+        //owner.transform.position = movement * Time.deltaTime;
+        Vector3.Lerp(owner.transform.position, playerPositionalDelay, 2f);
+
+        if (Vector3.Distance(owner.transform.position, playerPositionalDelay) <= 3f)
+            owner.ChangeState<BoomerChase>();
+    }
+
+    public override void ExitState()
+    {
+        base.ExitState();
+        owner.agent.enabled = true;
+    }
+
+    IEnumerator windUp()
+    {
+        yield return new WaitForSeconds(jumpWindupTime);
+    }
+}
+
+#region ChaseLegacy
+//Stagger(magnitude);
+//protected override void OperateHesitation()
+//{
+//    base.OperateHesitation();
+//    if (Vector3.Distance(owner.gameObject.transform.position, owner.target.gameObject.transform.position) <= hesitationDistance)
+//    {
+//        owner.ChangeState<FanaticHesitate>();
+//    }
+//}
+// lightAngle = lightField.spotAngle;
+//ChaseEvent chaseEvent = new ChaseEvent();
+//chaseEvent.gameObject = owner.gameObject;
+//chaseEvent.eventDescription = "Chasing Enemy";
+//chaseEvent.audioSpeaker = audioSpeaker;
+
+//EventSystem.Current.FireEvent(chaseEvent);
+#endregion
