5fdf85a1218efc094439d1341b4dbe921b803e38
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2019-11-19 16:56:07 +0100

    boomer jump

--- PROJ_P/Assets/Scripts/AI/Opponents/ChaseScripts/JumpImpact.cs
+++ PROJ_P/Assets/Scripts/AI/Opponents/ChaseScripts/JumpImpact.cs
@@ -10,15 +10,17 @@ using UnityEngine.AI;
 public class JumpImpact : AbilityBase
 {
     private GameObject jumpTimer, graphicalPrefab;
-    private float jumpWindupTime = 3f;
-    private float jumpSpeed = 3f, jumpHeight = 4f;
+    [SerializeField] private float jumpWindupTime = 3f;
+    private float jumpSpeed = 3f, jumpHeight = 10f;
     private Vector3 playerPositionalDelay;
     private Vector3 direction;
     private Vector3 movement;
-    private float maxHeight = 10f;
+    private Vector3 initialPosition;
     private float currentHeight;
     private bool jumping;
     private float startDistance, jumpThreshhold, distance;
+    private Transform mesh;
+    private const float upwardsMomentum = 0.03f, downwardsMomentum = 0.1f;
 
     public override void EnterState()
     {
@@ -32,17 +34,19 @@ public class JumpImpact : AbilityBase
         startDistance = Vector3.Distance(owner.agent.transform.position, playerPositionalDelay);
         //Likely to land too far above the ground // This is wrong, says unit should jump from above where it is currently standing
         //direction = playerPositionalDelay.forward * -1;
-
-
-
-        Debug.Log("Jumping");
+        mesh = owner.agent.transform.GetChild(4);
+        initialPosition = owner.agent.transform.position;
+        Debug.Log(mesh);
     }
 
     public override void ToDo()
     {
         base.ToDo();
         if (jumping)
+        {
             Jump();
+            CheckForDamage();
+        }
     }
 
     protected override void Die()
@@ -63,35 +67,44 @@ public class JumpImpact : AbilityBase
     protected override void ExecuteAbility()
     {
         base.ExecuteAbility();
+        if (!jumpTimer)
+        {
         jumpTimer = Instantiate(new GameObject("JumpTimer"));
         jumpTimer.AddComponent<Timer>().RunCountDown(jumpWindupTime, ActivateJump, Timer.TimerType.DELAY);
+        }
     }
 
     private void ActivateJump()
     {
         jumping = true;
     }
+
     private void Jump()
     {
-        Mathf.Clamp(currentHeight, 0, maxHeight);
+
         distance = Vector3.Distance(owner.agent.transform.position, playerPositionalDelay);
-        owner.agent.transform.LookAt(playerPositionalDelay);
-        owner.agent.SetDestination(new Vector3(playerPositionalDelay.x, playerPositionalDelay.y + maxHeight, playerPositionalDelay.z));
-
-        if(distance <= (startDistance / 2))
-        {
-        owner.agent.ResetPath();
+        //Debug.Log("distance" + " " + distance);
         owner.agent.SetDestination(playerPositionalDelay);
-        }
 
-        if (Vector3.Distance(owner.transform.position, playerPositionalDelay) <= 2f)
+        //if(distance >= 10f)
+
+        mesh.transform.position = Vector3.Lerp(owner.agent.transform.position, new Vector3(owner.agent.transform.position.x, owner.agent.transform.position.y + 20, owner.agent.transform.position.z), Time.deltaTime * 2f);
+
+
+        if (distance <= 3f)
+        {
+            mesh.transform.position = Vector3.Lerp(mesh.transform.position, new Vector3(mesh.transform.position.x, owner.capsuleCollider.radius, mesh.transform.position.z), Time.deltaTime * 2f);
             owner.ChangeState<BoomerChase>();
+        }
     }
+
+
 
     public override void ExitState()
     {
         base.ExitState();
         jumping = false;
+
     }
 
     IEnumerator windUp()
