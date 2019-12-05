8f14ff8e974c5460eacce4366239f39e4a2eeabf
Alexander Castman <alca3002@win.dsv.su.se>
2019-12-05 15:19:17 +0100

    Shopkeeper in text

---
+++ PROJ_P/Assets/RawImageSize.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageSize : MonoBehaviour
+{
+    private RawImage raw;
+
+    public void Start()
+    {
+        raw = GetComponent<RawImage>();
+    }
+    public void Update()
+    {
+        Resize(raw);
+    }
+
+    public Vector2 Resize(RawImage image, float padding = 0)
+    {
+        RectTransform parent = image.transform.parent.GetComponent<RectTransform>();
+        RectTransform imageTransform = image.GetComponent<RectTransform>();
+
+        if (!parent) { return imageTransform.sizeDelta; } //if we don't have a parent, just return our current width;
+        padding = 1 - padding;
+
+        float w = 0, h = 0;
+        float ratio = image.texture.width / (float)image.texture.height;
+        Rect bounds = new Rect(0, 0, parent.rect.width, parent.rect.height);
+
+        if (Mathf.RoundToInt(imageTransform.eulerAngles.z) % 180 == 90)
+        {
+            //Invert the bounds if the image is rotated
+            bounds.size = new Vector2(bounds.height, bounds.width);
+        }
+
+        //Size by height first
+        h = bounds.height * padding;
+        w = h * ratio;
+
+        if (w > bounds.width * padding)
+        { //If it doesn't fit, fallback to width;
+            w = bounds.width * padding;
+            h = w / ratio;
+        }
+
+        imageTransform.sizeDelta = new Vector2(w, h);
+        return imageTransform.sizeDelta;
+    }
+
+
+}
