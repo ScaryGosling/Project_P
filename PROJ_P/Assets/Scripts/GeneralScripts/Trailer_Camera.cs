ffe4cc39e8c1408d9e994ddb11c3eade1d541594
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2019-12-05 15:12:35 +0100

    Skybox + First trailer_clip

---
+++ PROJ_P/Assets/Scripts/GeneralScripts/Trailer_Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Trailer_Camera : MonoBehaviour
+{
+    private Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
