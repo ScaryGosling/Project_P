8a6e2f1e549358597928974d2a5a9f0f859d4298
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-12-09 13:23:41 +0100

    dialogue typewriter effect

---
+++ PROJ_P/Assets/Scripts/UI/DialogueEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueEffect : MonoBehaviour
+{
+
+    private string dialogueText;
+    private Text text;
+    [SerializeField] private float typeSpeed = 0.015f;
+
+
+
+    public void SetDialogue(Text textObject, string dialogueText)
+    {
+        StopCoroutine("PlayText");
+        text = textObject;
+        this.dialogueText = dialogueText;
+        text.text = "";
+        StartCoroutine("PlayText");
+    }
+
+
+    IEnumerator PlayText()
+    {
+
+        foreach (char c in dialogueText)
+        {
+            text.text += c;
+
+            yield return new WaitForSeconds(typeSpeed);
+        }
+
+    }
+
+}
