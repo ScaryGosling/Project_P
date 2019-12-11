ebf6be6ca07c2b99bc5575c7f03efc856d5b76a8
paschalisTol <37561700+paschalistol@users.noreply.github.com>
2019-12-11 17:19:48 +0100

    highscore scene

---
+++ PROJ_P/Assets/Scripts/UI/ScoreboardEntryUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreboardEntryUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI entryNameText = null;
+    [SerializeField] private TextMeshProUGUI entryScoreText = null;
+
+    public void Initialize(ScoreboardEntryData scoreboardEntry)
+    {
+        entryNameText.text = scoreboardEntry.entryName;
+        entryScoreText.text = scoreboardEntry.entryScore.ToString();
+    }
+}
---
+++ PROJ_P/Assets/Scripts/UI/ScoreboardSaveData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class ScoreboardSaveData 
+{
+    public List<ScoreboardEntryData> highscores = new List<ScoreboardEntryData>();
+   
+}
