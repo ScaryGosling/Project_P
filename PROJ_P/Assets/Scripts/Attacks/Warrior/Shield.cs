6bd44f3f53ed8951184b5bda365d02b50a3e095d
theodorwin333 <37655604+theodorwin333@users.noreply.github.com>
2019-12-16 15:44:07 +0100

    balance update

--- PROJ_P/Assets/Scripts/Attacks/Warrior/Shield.cs
+++ PROJ_P/Assets/Scripts/Attacks/Warrior/Shield.cs
@@ -17,7 +17,7 @@ public class Shield : PlayerAttack
         shield.transform.position += new Vector3(0,0.5f,0);
         player.activeStats.resistanceMultiplier = 0;
 
-        player.AnimationTrigger("Shield");
+        //player.AnimationTrigger("Shield");
 
         BowoniaPool.instance.GetFromPool(PoolObject.TIMER).GetComponent<Timer>().RunCountDown(duration, RemoveShield, Timer.TimerType.DELAY);
     }
