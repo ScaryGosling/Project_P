7b6c8871dac699d34ead76c792f1d40b214cd1e9
Paschalis Tolios <37561700+paschalistol@users.noreply.github.com>
2019-12-20 14:13:23 +0100

    show health/resource bar in shop

---
+++ PROJ_P/Assets/Scripts/Menus/Shop/FillBarManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillBarManager : MonoBehaviour
+{
+
+    [SerializeField] private Image healthBar, resourceBar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = Player.instance.HealthProp * 0.01f;
+        resourceBar.fillAmount = Player.instance.Resource.Value;
+    }
+}
