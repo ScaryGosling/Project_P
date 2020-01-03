95db6d02faee19f1193049691d67dd111ff916ea
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2020-01-03 22:37:51 +0100

    Prologue style change & exit prologue

--- PROJ_P/Assets/Scripts/UI/Dialogue.cs
+++ PROJ_P/Assets/Scripts/UI/Dialogue.cs
@@ -48,6 +48,12 @@ public class Dialogue : MonoBehaviour
     }
     private void Update()
     {
+        if (Input.GetKeyDown(settings.GetBind(KeyFeature.TOGGLE_SHOP)))
+        {
+            TerminateDialogue();
+            return;
+        }
+
         if ((Input.GetKeyDown(settings.GetBind(KeyFeature.DIALOGUE_FORWARD)) && dialogueActive))
             Next(DialogueDirection.FORWARD);
         else if (Input.GetKeyDown(settings.GetBind(KeyFeature.DIALOGUE_BACK)) && dialogueActive)
@@ -66,7 +72,7 @@ public class Dialogue : MonoBehaviour
     {
         text.text = dialogueField;
         PlayNextMessage();
-        if (sprites != null && sprites.Length > 0)
+        if (sprites != null && sprites.Length > 0 && sprites[n])
             image.sprite = sprites[n];
 
         if (DialogueProp == DialogueType.TUTORIAL)
